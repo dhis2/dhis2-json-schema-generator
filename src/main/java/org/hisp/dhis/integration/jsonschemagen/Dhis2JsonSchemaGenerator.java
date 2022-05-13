bdf3017bd507a4c18246f9125191a041caa899fc
Claude Mamo <823038+claudemamo@users.noreply.github.com>
2022-05-13 06:53:55 +0200

    Generate opaque schemas for non-Jackson annotated classes (#14)
    * refactor: don't introspect non-Jackson annotated classes and instead generate inline object schemas without any properties so that (1) a lot of corner cases can be dropped from the code and (2) implementation-specific schemas like `LinkedHashMap.json` are not generated
    
    * test: clear state at the start of each test because of failures in CI
    
    * ci: updated to codeql-action v2 because of warning
    
    * ci: updated to codeql-action v2 because of warning

--- src/main/java/org/hisp/dhis/integration/jsonschemagen/Dhis2JsonSchemaGenerator.java
+++ src/main/java/org/hisp/dhis/integration/jsonschemagen/Dhis2JsonSchemaGenerator.java
@@ -29,10 +29,9 @@ package org.hisp.dhis.integration.jsonschemagen;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -63,7 +62,8 @@ public final class Dhis2JsonSchemaGenerator
     }
 
     public static void main( String[] args )
-        throws IOException
+        throws IOException,
+        URISyntaxException
     {
         String outputDestinationDir = args[0];
 
@@ -77,19 +77,11 @@ public final class Dhis2JsonSchemaGenerator
 
         JacksonModule jacksonModule = new JacksonModule( JacksonOption.INCLUDE_ONLY_JSONPROPERTY_ANNOTATED_METHODS,
             JacksonOption.RESPECT_JSONPROPERTY_REQUIRED );
+
         for ( Class<?> apiClass : apiClasses )
         {
-            generateJsonSchema( apiClass, jacksonModule, true, outputDestinationDir );
+            generateJsonSchema( apiClass, jacksonModule, outputDestinationDir );
         }
-
-        generateJsonSchema( getClassOrNull( "org.locationtech.jts.geom.Geometry" ), jacksonModule, false,
-            outputDestinationDir );
-        generateJsonSchema( getClassOrNull( "org.hisp.dhis.common.DimensionalItemObject" ), jacksonModule, false,
-            outputDestinationDir );
-        generateJsonSchema( getClassOrNull( "org.hisp.dhis.common.DimensionalObject" ), jacksonModule, false,
-            outputDestinationDir );
-        generateJsonSchema( getClassOrNull( "org.hisp.dhis.common.DimensionItemKeywords" ), jacksonModule, false,
-            outputDestinationDir );
     }
 
     static List<Class<?>> prepare( Set<Class<?>>... types )
@@ -102,24 +94,13 @@ public final class Dhis2JsonSchemaGenerator
 
         return apiClasses.stream()
             .filter( a -> !(a.getName().startsWith( "com.fasterxml" ) || a.getName()
-                .startsWith( "org.hisp.dhis.schema.annotation.Property" )) ).sorted(Comparator.comparing( Class::getName ))
+                .startsWith( "org.hisp.dhis.schema.annotation.Property" )) )
+            .sorted( Comparator.comparing( Class::getName ) )
             .collect( Collectors.toList() );
     }
 
-    private static Class<?> getClassOrNull( String className )
-    {
-        try
-        {
-            return Class.forName( className );
-        }
-        catch ( ClassNotFoundException e )
-        {
-            return null;
-        }
-    }
-
     private static void generateJsonSchema( Class<?> apiClass, JacksonModule jacksonModule,
-        boolean discardEmptySchemas, String outputDestinationDir )
+        String outputDestinationDir )
         throws IOException
     {
         if ( apiClass == null )
@@ -127,84 +108,37 @@ public final class Dhis2JsonSchemaGenerator
             return;
         }
 
-        SchemaGeneratorConfigBuilder configBuilder = new SchemaGeneratorConfigBuilder(
-            SchemaVersion.DRAFT_2019_09,
+        SchemaGeneratorConfigBuilder schemaGeneratorConfigBuilder = new SchemaGeneratorConfigBuilder(
+            SchemaVersion.DRAFT_2020_12,
             OptionPreset.PLAIN_JSON )
-            .with( jacksonModule )
-            .with( Option.PLAIN_DEFINITION_KEYS, Option.GETTER_METHODS, Option.MAP_VALUES_AS_ADDITIONAL_PROPERTIES,
-                Option.FIELDS_DERIVED_FROM_ARGUMENTFREE_METHODS, Option.NONSTATIC_NONVOID_NONGETTER_METHODS )
-            .without( Option.NONPUBLIC_NONSTATIC_FIELDS_WITH_GETTERS, Option.EXTRA_OPEN_API_FORMAT_VALUES );
+                .with( jacksonModule )
+                .with( Option.PLAIN_DEFINITION_KEYS, Option.GETTER_METHODS, Option.MAP_VALUES_AS_ADDITIONAL_PROPERTIES,
+                    Option.FIELDS_DERIVED_FROM_ARGUMENTFREE_METHODS, Option.NONSTATIC_NONVOID_NONGETTER_METHODS )
+                .without( Option.NONPUBLIC_NONSTATIC_FIELDS_WITH_GETTERS, Option.EXTRA_OPEN_API_FORMAT_VALUES );
 
-        configBuilder.forTypesInGeneral().withStringFormatResolver( target -> {
-            if ( target.getType().getErasedType().equals( Date.class ) )
-            {
-                return "date-time";
-            }
-            else
-            {
-                return null;
-            }
-        } ).withCustomDefinitionProvider( new RefDefinitionProvider( apiClass ) );
-        configBuilder.forFields()
+        schemaGeneratorConfigBuilder.forTypesInGeneral().withStringFormatResolver( new DateStringFormatResolver() )
+            .withCustomDefinitionProvider( new RefDefinitionProvider( apiClass ) );
+        schemaGeneratorConfigBuilder.forFields()
             .withIgnoreCheck( fieldScope -> fieldScope.getAnnotation( JsonProperty.class ) == null );
 
-        SchemaGeneratorConfig config = configBuilder.build();
-        SchemaGenerator generator = new SchemaGenerator( config,
-            TypeContextFactory.createTypeContext( AnnotationInclusion.INCLUDE_AND_INHERIT, config ) );
-        ObjectNode jsonSchema = generator.generateSchema( apiClass );
+        SchemaGeneratorConfig schemaGeneratorConfig = schemaGeneratorConfigBuilder.build();
+        SchemaGenerator schemaGenerator = new SchemaGenerator( schemaGeneratorConfig,
+            TypeContextFactory.createTypeContext( AnnotationInclusion.INCLUDE_AND_INHERIT, schemaGeneratorConfig ) );
+        ObjectNode jsonSchema = schemaGenerator.generateSchema( apiClass );
 
-        Set<Class<?>> includedApiClasses = new HashSet<>();
-        includeApiClass( "org.hisp.dhis.sms.outbound.BulkSmsRecipient", includedApiClasses );
-        includeApiClass( "org.hisp.dhis.period.PeriodType", includedApiClasses );
-        includeApiClass( "org.hisp.dhis.dxf2.webmessage.WebMessageResponse", includedApiClasses );
-        includeApiClass( "org.hisp.dhis.common.IdSchemes", includedApiClasses );
-        includeApiClass( "org.hisp.dhis.scheduling.JobParameters", includedApiClasses );
-        includeApiClass( "org.hisp.dhis.common.NameableObject", includedApiClasses );
-
-        if ( !discardEmptySchemas || Map.class.isAssignableFrom( apiClass ) || includedApiClasses.contains( apiClass )
-            || jsonSchema.get( "const" ) != null
-            || jsonSchema.get( "enum" ) != null
-            || jsonSchema.get( "properties" ) != null )
+        if ( RefToTypeMapping.getInstance().containsValue( apiClass ) )
         {
             String filename = null;
-            if ( RefToTypeMapping.getInstance().containsValue( apiClass ) )
+            for ( Map.Entry<String, Class<?>> mapping : RefToTypeMapping.getInstance().entrySet() )
             {
+                if ( mapping.getValue().equals( apiClass ) )
+                {
+                    filename = mapping.getKey();
+                }
+            }
 
-                for ( Map.Entry<String, Class<?>> mapping : RefToTypeMapping.getInstance().entrySet() )
-                {
-                    if ( mapping.getValue().equals( apiClass ) )
-                    {
-                        filename = mapping.getKey();
-                    }
-                }
-            }
-            else
-            {
-                int repeat = 0;
-                while ( filename == null || (RefToTypeMapping.getInstance().get( filename ) != null) )
-                {
-                    filename = apiClass.getSimpleName().substring( 0, 1 )
-                        .toLowerCase()
-                        + apiClass.getSimpleName().substring( 1 )
-                        + "_".repeat( repeat ) + ".json";
-                    repeat++;
-                }
-                RefToTypeMapping.getInstance().put( filename, apiClass );
-            }
             FileUtils.writeStringToFile( new File( outputDestinationDir + "/" + filename ), jsonSchema.toPrettyString(),
                 "UTF-8" );
-        }
-    }
-
-    private static void includeApiClass( String apiClassName, Set<Class<?>> includedApiClasses )
-    {
-        try
-        {
-            includedApiClasses.add( Class.forName( apiClassName ) );
-        }
-        catch ( ClassNotFoundException ignored )
-        {
-
         }
     }
 }
